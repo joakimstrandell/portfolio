0ba7c51f811c505b52a91f2b63515725fbef6731
Joakim Strandell <joakim@awkwardgroup.com>
2026-01-30 14:26:27 +0100

    feat(button): add xl size variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -32,6 +32,7 @@ const buttonVariants = cva(
         default: 'h-10 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'text-md h-12 px-6 has-[>svg]:px-4',
+        xl: 'h-14 px-8 text-lg has-[>svg]:px-6',
         icon: 'size-9',
         'icon-sm': 'size-8',
         'icon-lg': 'size-10',
